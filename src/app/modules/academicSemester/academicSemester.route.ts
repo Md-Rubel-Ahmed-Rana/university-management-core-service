54fc0a02fd15f9998590e5b0d41baba0b15a10b0
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-08-29 23:52:39 +0600

    validation done

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import validateRequest from "../../middlewares/validateRequest";
 import { AcademicSemesterController } from "./academicSemester.controller";
+import { AcademicSemesterValidator } from "./academicSemester.validator";
 
 const router = Router();
 
-router.post("/", AcademicSemesterController.insertIntoDB);
+router.post("/", validateRequest(AcademicSemesterValidator.createZod) , AcademicSemesterController.insertIntoDB);
 
 
 export const AcademicSemesterRoutes = router

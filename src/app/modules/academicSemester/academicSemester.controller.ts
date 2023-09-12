1426b6e6a16d53cbe2604cf1a55e0049c5136cf6
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2023-09-13 01:11:39 +0600

    Filtering and searching completed

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,39 +1,41 @@
 import { AcademicSemester } from '@prisma/client';
-import { Request, Response } from "express";
-import httpStatus from "http-status";
-import catchAsync from "../../../shared/catchAsync";
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
 import pick from '../../../shared/pick';
-import sendResponse from "../../../shared/sendResponse";
-import { AcademicSemesterService } from "./academicSemester.service";
+import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterService } from './academicSemester.service';
 
-const insertIntoDB = catchAsync(async(req: Request, res: Response) => {
-    const result = await AcademicSemesterService.insertIntoDB(req.body);
-    sendResponse<AcademicSemester>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Academic Semester created",
-        data: result
-    })
-})
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester created',
+    data: result,
+  });
+});
 
-
-
-const getAllFromDB = catchAsync(async(req: Request, res: Response) => {
-    const filters = pick(req.query, ["searchTerm", "code", "year"])
-    const options = pick(req.query, ["page", "limit", "skip", "sortBy", "sortOrder"])
-    const result = await AcademicSemesterService.getAllFromDB(filters, options);
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Academic Semester created",
-        meta: result.meta,
-        data: result.data
-    })
-})
-
-
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, ['searchTerm', 'code', 'year']);
+  const options = pick(req.query, [
+    'page',
+    'limit',
+    'skip',
+    'sortBy',
+    'sortOrder',
+  ]);
+  const result = await AcademicSemesterService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semesters retrieved successfully!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
 export const AcademicSemesterController = {
-    insertIntoDB,
-    getAllFromDB
-}
+  insertIntoDB,
+  getAllFromDB,
+};
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,41 +1,79 @@
-import { AcademicSemester, PrismaClient } from "@prisma/client";
-import { paginationHelpers } from "../../../helpers/paginationHelper";
-import { IGenericResponse } from "../../../interfaces/common";
-import { IPaginationOptions } from "../../../interfaces/pagination";
+import { AcademicSemester, Prisma, PrismaClient } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { IAcademicSemesterFilter } from './academicSemester.interface';
 
 const prisma = new PrismaClient();
 
-const insertIntoDB = async(data: AcademicSemester): Promise<AcademicSemester> =>{
-    const result = await prisma.academicSemester.create({
-        data
-    })
-    return result
-}
+const insertIntoDB = async (
+  data: AcademicSemester
+): Promise<AcademicSemester> => {
+  const result = await prisma.academicSemester.create({
+    data,
+  });
+  return result;
+};
 
-const getAllFromDB = async(
-    filters, 
-    options: IPaginationOptions
-    ): 
-    Promise<IGenericResponse<AcademicSemester[]>> 
-    =>{
-        const {page, limit, skip} = paginationHelpers.calculatePagination(options)
-    const result = await prisma.academicSemester.findMany({
-        skip,
-        take: limit
-    });
-    const total = await prisma.academicSemester.count()
-    return {
-        meta: {
-            total,
-            page,
-            limit
+const getAllFromDB = async (
+  filters: IAcademicSemesterFilter,
+  options: IPaginationOptions
+): Promise<IGenericResponse<AcademicSemester[]>> => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+
+  const { searchTerm, ...filterData } = filters;
+
+  const andCondition = [];
+  if (searchTerm) {
+    andCondition.push({
+      OR: ['title', 'code', 'startMonth', 'endMonth'].map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
         },
-        data: result
-    }
-}
+      })),
+    });
+  }
 
+  // filtering
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: filterData[key] as string,
+        },
+      })),
+    });
+  }
+
+  const whereCondition: Prisma.AcademicSemesterWhereInput =
+    andCondition.length > 0 ? { AND: andCondition } : {};
+
+  const result = await prisma.academicSemester.findMany({
+    where: whereCondition,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.academicSemester.count();
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
 
 export const AcademicSemesterService = {
-    insertIntoDB,
-    getAllFromDB
-}
+  insertIntoDB,
+  getAllFromDB,
+};
